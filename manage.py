373f7f415c0052064f8d3a00d01e02f9b00d1146
junyaoren <renjunyao@gmail.com>
2018-10-06 03:58:44 -0700

    reorganized code

--- manage.py
+++ manage.py
@@ -1,12 +1,7 @@
 from flask_script import Manager
-from flask_migrate import Migrate, MigrateCommand
-from app import db, app
-
-
+from app import app
 
 manager = Manager(app)
-migrate = Migrate(app, db)
-manager.add_command('db', MigrateCommand)
 
 
 if __name__ == "__main__":
