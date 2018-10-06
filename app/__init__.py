b3963cfb389b4e7272b54d6efaad191a7590c2de
junyaoren <renjunyao@gmail.com>
2018-10-06 03:48:45 -0700

    reorganized code

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
 # third-party imports
+import os
 from flask import Flask
 from flask_bootstrap import Bootstrap
 from flask_login import LoginManager
@@ -11,30 +12,31 @@ from config import app_config
 db = SQLAlchemy()
 login_manager = LoginManager()
 
+config_name = os.getenv('FLASK_CONFIG')
+config_name = 'production'
 
-def create_app(config_name):
-    app = Flask(__name__, instance_relative_config=True)
-    app.config.from_object(app_config[config_name])
-    app.config.from_pyfile('config.py')
+app = Flask(__name__, instance_relative_config=True)
+app.config.from_object(app_config[config_name])
+app.config.from_pyfile('config.py')
 
-    Bootstrap(app)
-    db.init_app(app)
-    login_manager.init_app(app)
-    login_manager.login_message = "You must be logged in to access this page."
-    login_manager.login_view = "auth.login"
-    migrate = Migrate(app, db)
+Bootstrap(app)
+db.init_app(app)
+login_manager.init_app(app)
+login_manager.login_message = "You must be logged in to access this page."
+login_manager.login_view = "auth.login"
+migrate = Migrate(app, db)
 
-    from app import models
+from app import models
 
-    from .admin import admin as admin_blueprint
-    app.register_blueprint(admin_blueprint, url_prefix='/admin')
+from .admin import admin as admin_blueprint
+app.register_blueprint(admin_blueprint, url_prefix='/admin')
 
-    from .auth import auth as auth_blueprint
-    app.register_blueprint(auth_blueprint)
+from .auth import auth as auth_blueprint
+app.register_blueprint(auth_blueprint)
 
-    from .home import home as home_blueprint
-    app.register_blueprint(home_blueprint)
+from .home import home as home_blueprint
+app.register_blueprint(home_blueprint)
 
-    return app
+
 
 
--- manage.py
+++ manage.py
@@ -1,11 +1,8 @@
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
-from app import db, create_app
-import os
+from app import db, app
 
-config_name = os.getenv('FLASK_CONFIG')
-config_name = 'production'
-app = create_app(config_name)
+
 
 manager = Manager(app)
 migrate = Migrate(app, db)
