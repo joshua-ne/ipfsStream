312b3e76cb296bd6d8afbaf2ace5cc8be46b0865
junyaoren <renjunyao@gmail.com>
2018-10-06 03:35:21 -0700

    reorganized code

--- run.py
+++ run.py
@@ -1,11 +1,8 @@
-import os
-from app import create_app
+from manage import app
 
 
 
 
 if __name__ == '__main__':
-    config_name = os.getenv('FLASK_CONFIG')
-    config_name = 'production'
-    app = create_app(config_name)
+
     app.run()
